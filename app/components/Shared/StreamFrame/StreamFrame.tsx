04047109e9271e10543efe1d135fbaeb031fb20a
vladilen597 <vladilen597@gmail.com>
2026-03-10 20:13:29 +0300

    fix: change twitch loading

--- app/components/Shared/StreamFrame/StreamFrame.tsx
+++ app/components/Shared/StreamFrame/StreamFrame.tsx
@@ -1,22 +1,29 @@
 import Script from "next/script";
+import { useEffect } from "react";
 
 const TwitchPlayer = () => {
+  const initPlayer = () => {
+    const container = document.getElementById("twitch-embed");
+    if ((window as any).Twitch && container && container.innerHTML === "") {
+      new (window as any).Twitch.Embed("twitch-embed", {
+        width: "100%",
+        height: 720,
+        channel: "araysee",
+        parent: ["jk-cup.vercel.app", "localhost"],
+        layout: "video",
+      });
+    }
+  };
+
+  useEffect(() => {
+    initPlayer();
+  }, []);
+
   return (
-    <>
-      <Script
-        src="https://embed.twitch.tv/embed/v1.js"
-        onLoad={() => {
-          new (window as any).Twitch.Embed("twitch-embed", {
-            width: "100%",
-            height: 720,
-            channel: "araysee",
-            parent: ["jk-cup.vercel.app", "localhost"],
-            layout: "video",
-          });
-        }}
-      />
+    <div>
+      <Script src="https://embed.twitch.tv/embed/v1.js" onLoad={initPlayer} />
       <div id="twitch-embed"></div>
-    </>
+    </div>
   );
 };
 
