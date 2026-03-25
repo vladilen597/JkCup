8e916ffceef45001a4d5c76a3d62c903e49e9705
vladilen597 <vladilen597@gmail.com>
2026-03-25 12:30:04 +0300

    feat: add bg to selected games in profile

--- app/(app)/users/[id]/profile/page.tsx
+++ app/(app)/users/[id]/profile/page.tsx
@@ -167,7 +167,7 @@ const page = () => {
                         userInfo?.games.map((game) => (
                           <div
                             key={game.id}
-                            className="flex bg-background/50 p-1.5 rounded-lg items-center gap-2"
+                            className="flex bg-card p-1.5 rounded-lg items-center gap-2"
                           >
                             {game?.image_url && (
                               <Image
